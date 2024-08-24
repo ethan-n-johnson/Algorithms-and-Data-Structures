57e376b3f465ba86605323cefd70a163a19877fa
Ethan Johnson <ethansoccer111@gmail.com>
2024-08-24 14:55:54 -0500

    checkpoint

--- lab1_ethan_johnson.c
+++ lab1_ethan_johnson.c
@@ -15,18 +15,32 @@ void print_array(Pair *array, int length){
     printf("\n");
 }
 
-// Compares two pair elements for use in Qsort
-int comp(const void* elem1, const void* elem2){
+// Compares two pair elements based on their value for use in Qsort
+int comp_value(const void* elem1, const void* elem2){
     Pair *pair1 = (Pair *)elem1;
     Pair *pair2 = (Pair *)elem2;
-    if (pair1->value == pair2->value) return pair1->initial_position - pair2->initial_position;
+    if (pair1->initial_position == pair2->initial_position) return pair1->initial_position - pair2->initial_position;
     else return pair1->value - pair2->value;
+}
+// Compares two pair elements based on their postition for use in Qsort
+int comp_position(const void* elem1, const void* elem2){
+    Pair *pair1 = (Pair *)elem1;
+    Pair *pair2 = (Pair *)elem2;
+    return pair1->initial_position - pair2->initial_position;
 }
 
 // Keep only the last occurence of a duplicate
 int remove_dupe(Pair* array, int length){
-    // Start at the end of the array and work backwords
     int new_length = length;
+    Pair temp;
+    for (int i = 0; i < length; i++) {
+        if (array[i].value == array[i+1].value){
+            array[i] = array[i+1];
+            temp = array[length-i];
+            new_length--;
+        }
+    }
+    return new_length;
 }
 
 int main(){
@@ -44,7 +58,7 @@ int main(){
     printf("Before Sorting:\n");
     print_array(array, array_length);
 
-    qsort(array, array_length, sizeof(Pair), comp);
+    qsort(array, array_length, sizeof(Pair), comp_value);
     printf("After Sorting:\n");
     print_array(array, array_length);
 
@@ -52,7 +66,7 @@ int main(){
     printf("Remove Extras:\n");
     print_array(array, new_length);
 
-    // qsort(array, new_length, sizeof(Pair), comp);
+    // qsort(array, new_length, sizeof(Pair), comp_position);
     // printf("After Sorting:\n");
     // print_array(array, new_length);
 }
